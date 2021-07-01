f7aab7b6955d529f7402a3497b16999d4d42c659
AliIman <aiman@uwaterloo.ca>
2021-07-01 17:27:27 -0400

    Fixed some of the indentation problems

--- src/test/java/org/joda/time/convert/TestConverterTestGetPartialValuesTemplate.java
+++ src/test/java/org/joda/time/convert/TestConverterTestGetPartialValuesTemplate.java
@@ -6,7 +6,7 @@ import static junit.framework.Assert.assertEquals;
 import java.util.Arrays;
 import static java.util.Arrays.equals;
 public class TestConverterTestGetPartialValuesTemplate {
-  public static <TObject>void testConverterTestGetPartialValuesTemplate(  Class<TObject> clazzTObject,  AbstractConverter abstractConverter1) throws Exception {
+  public static <TObject> void testConverterTestGetPartialValuesTemplate(Class<TObject> clazzTObject,  AbstractConverter abstractConverter1) throws Exception {
     TimeOfDay tod=new TimeOfDay();
     int[] expected=ISOChronology.getInstance().get(tod,12345678L);
     int[] actual=abstractConverter1.getPartialValues(tod,(Object)clazzTObject.getDeclaredConstructor(long.class).newInstance(12345678L),ISOChronology.getInstance());
